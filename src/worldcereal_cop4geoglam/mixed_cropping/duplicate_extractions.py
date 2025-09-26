1d5a25eec4dd0e4ddf923d7579f787cef7ddaa27
DEVOSK <koen.devos@vito.be>
2025-09-26 15:05:02 +0200

    duplicates

--- src/worldcereal_cop4geoglam/mixed_cropping/duplicate_extractions.py
+++ src/worldcereal_cop4geoglam/mixed_cropping/duplicate_extractions.py
@@ -3,6 +3,7 @@ Main Code to duplicate extractions for mixed cropping points.
 #►Copernicus4GEOGLAM
 '''
 
+import glob
 import os
 from json import dump, load
 
@@ -13,8 +14,10 @@ activation = "mozambique"
 
 base_folder = "/vitodata/worldcereal/data/COP4GEOGLAM/"
 activation_folder = os.path.join(base_folder, activation)
-extractions = os.path.join(activation_folder,
-                           "trainingdata","worldcereal_merged_extractions.parquet")
+extractions = glob.glob(os.path.join(activation_folder,
+                           "trainingdata","worldcereal_merged_extractions.parquet",
+                           "*EXP*",
+                           "*.parquet"))[0]
 #get the duplicate mapping json from the same folder as this script
 duplicate_mapping_file = os.path.join(activation_folder,"mixed_cropping",
                                       f"duplicate_mapping_{activation.capitalize()}.json")
@@ -30,6 +33,10 @@ with open(class_mapping_file, "r") as f:
     class_mapping = load(f)
 croptype_mapping = class_mapping[f"CROPTYPE_{activation.capitalize()}"]
 
+keys_to_remove = ["1114020031", "1114060011", "1114060012"]
+for key in keys_to_remove:
+    croptype_mapping.pop(key, None)
+
 duplicate_croptype_mapping = {}
 for key, value in croptype_mapping.items():
     if value not in duplicate_croptype_mapping:
@@ -41,13 +48,12 @@ original_gpkg = os.path.join(activation_folder,
 extractions_dataset = gpd.read_parquet(extractions)
 original_dataset = gpd.read_file(original_gpkg)
 
-extractions_dataset["round_x"] = extractions_dataset["geometry"].x.round(4)
-extractions_dataset["round_y"] = extractions_dataset["geometry"].y.round(4)
+extractions_dataset["id_ssu"] = [
+    f"{sample_id.split('_')[5]}_{sample_id.split('_')[6]}"
+    for sample_id in extractions_dataset["sample_id"]
+]
 
-original_dataset["round_x"] = original_dataset["geometry"].x.round(4)
-original_dataset["round_y"] = original_dataset["geometry"].y.round(4)
-
-merged = extractions_dataset.merge(original_dataset, on=["round_x", "round_y"],
+merged = extractions_dataset.merge(original_dataset, on=["id_ssu"],
                                    how="left", suffixes=("", "_original"))
 
 croptypes = merged["croptype"].unique()
@@ -59,11 +65,12 @@ for croptype in croptypes:
     merged_c = merged[merged["croptype"] == croptype]
     if croptype in list(duplicate_mapping_final.keys()):
         print("duplicating croptype:", croptype)
-        sample_ids = merged_c["sample_id"].unique()
+        sample_ids = merged_c["id_ssu"].unique()
         duplicates = duplicate_mapping_final[croptype]
         duplicate_ids.append(sample_ids)
-        for duplic in duplicates:
+        for d,duplic in enumerate(duplicates):
             merged_d = merged_c.copy()
+            merged_d["sample_id"] = [sid + f"_D{d+1}" for sid in merged_d["sample_id"]]
             merged_d["croptype"] = duplic
             merged_d["ewoc_code"] = int(duplicate_croptype_mapping[duplic])
             duplicated_list.append(merged_d)
@@ -71,17 +78,24 @@ for croptype in croptypes:
         duplicated_list.append(merged_c)
 
 duplicate_ids = [item for sublist in duplicate_ids for item in sublist]
+duplicate_ids = list(set(duplicate_ids))
 
 result = gpd.GeoDataFrame(pd.concat(duplicated_list, ignore_index=True),
                           geometry="geometry",crs=extractions_dataset.crs)
 result = result.loc[:,list(extractions_dataset.columns)]
-result = result.drop(columns=["round_x", "round_y"])
+
+extractions_notcopied = extractions_dataset[~extractions_dataset["id_ssu"].isin(
+    duplicate_ids)]
+
+result = gpd.GeoDataFrame(pd.concat([result, extractions_notcopied], ignore_index=True),
+                            geometry="geometry",crs=extractions_dataset.crs)
 
 #write duplicate_ids to a json file
 duplicate_ids_file = os.path.join(activation_folder, "mixed_cropping",
-                                  f"duplicate_ids_{activation}.json")
-output_file = os.path.join(activation_folder, "mixed_cropping",
-                           f"worldcereal_merged_extractions_{activation}_duplicated.parquet")
+                                  f"duplicate_ids_{activation}_with_EXT.json")
+output_file = os.path.join(
+    activation_folder, "mixed_cropping",
+    f"worldcereal_merged_extractions_{activation}_duplicated_with_EXT.parquet")
 
 result.to_parquet(output_file, index=False)
 print(f"Duplicated extractions saved to {output_file}")
