eb0a51d14cb59a76d4e713b5896990d520714446
giollimirgia <giorgia.milli@vito.be>
2025-10-09 14:52:03 +0200

    path in hpc

--- src/worldcereal_cop4geoglam/constants.py
+++ src/worldcereal_cop4geoglam/constants.py
@@ -1,9 +1,9 @@
 from typing import Dict, List
 
 PRESTO_PRETRAINED_MODEL_PATH: Dict[str, str] = {
-    "LANDCOVER" : "/vitodata/worldcereal/models/WorldCerealPresto/presto-prometheo-landcover-month-LANDCOVER10-augment=True-balance=True-timeexplicit=False-run=202507170930/presto-prometheo-landcover-month-LANDCOVER10-augment=True-balance=True-timeexplicit=False-run=202507170930_encoder.pt",
-    "CROPTYPE" : "/vitodata/worldcereal/models/WorldCerealPresto/presto-prometheo-landcover-month-CROPTYPE27-augment=True-balance=True-timeexplicit=True-run=202507181013/presto-prometheo-landcover-month-CROPTYPE27-augment=True-balance=True-timeexplicit=True-run=202507181013_encoder.pt",
-    "DEFAULT" : "https://artifactory.vgt.vito.be/artifactory/auxdata-public/worldcereal/models/PhaseII/presto-ss-wc_longparquet_random-window-cut_no-time-token_epoch96.pt"
+    "LANDCOVER": "/projects/worldcereal/COP4GEOGLAM/presto-prometheo-landcover-month-LANDCOVER10-augment=True-balance=True-timeexplicit=False-run=202507170930_encoder.pt",
+    "CROPTYPE": "/projects/worldcereal/COP4GEOGLAM/presto-prometheo-landcover-month-CROPTYPE27-augment=True-balance=True-timeexplicit=True-run=202507181013_encoder.pt",
+    "DEFAULT": "https://artifactory.vgt.vito.be/artifactory/auxdata-public/worldcereal/models/PhaseII/presto-ss-wc_longparquet_random-window-cut_no-time-token_epoch96.pt"
 }
 
 COUNTRY_PARQUET_FILES: Dict[str, Dict[str, List[str]]] = {
@@ -25,7 +25,7 @@ COUNTRY_PARQUET_FILES: Dict[str, Dict[str, List[str]]] = {
     "mozambique": {
         "month": [
             # "/vitodata/worldcereal/data/COP4GEOGLAM/mozambique/trainingdata/worldcereal_merged_extractions.parquet/ref_id=2025_MOZ_COPERNICUS4GEOGLAM_POINT_110_harmonized_20250919_1",
-            "/vitodata/worldcereal/data/COP4GEOGLAM/mozambique/trainingdata/worldcereal_merged_extractions.parquet/ref_id=2025_MOZ_COPERNICUS4GEOGLAM_POINT_110_harmonized_with_EXP_POINTS",
+            "/projects/worldcereal/COP4GEOGLAM/mozambique/worldcereal_merged_extractions.parquet/ref_id=2025_MOZ_COPERNICUS4GEOGLAM_POINT_110_harmonized_with_EXP_POINTS",
             # "/vitodata/worldcereal/data/COP4GEOGLAM/mozambique/trainingdata/worldcereal_merged_extractions_no_agroforestry.parquet",
             # "/vitodata/worldcereal/data/COP4GEOGLAM/mozambique/mixed_cropping/worldcereal_merged_extractions_mozambique_duplicated_with_EXT.parquet"
         ],
