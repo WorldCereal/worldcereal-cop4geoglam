0d67a99c0369cdf1bc169a7e38eb6842ad4dc179
devosk <koen.devos@vito.be>
2026-05-13 12:07:34 +0200

    updates on pruning

--- scripts/distance_feature_space.py
+++ scripts/distance_feature_space.py
@@ -23,6 +23,7 @@ pigeon_pea_points = df[df["ewoc_code"] == 1105010060]
 pigeon_pea_points[pigeon_pea_points == 65535] = pd.NA
 
 maize_mean = maize_points[pc_cols].median()
+pgp_mean = pigeon_pea_points[pc_cols].median()
 
 #calculate the euclidean distance of each pigeon pea point to the maize mean point
 def euclidean_distance(row, mean):
@@ -35,3 +36,10 @@ pigeon_pea_ssu = pigeon_pea_points.groupby("ssu_id")["distance_to_maize"].mean()
 
 outfile = "/vitodata/worldcereal/data/COP4GEOGLAM/mozambique_pm/feature_distance/pigeon_pea_distance_to_maize.parquet"
 pigeon_pea_ssu.to_parquet(outfile)
+
+maize_points["distance_to_pigeon_pea"] = maize_points[pc_cols].apply(lambda row: euclidean_distance(row, pgp_mean), axis=1)
+maize_points["ssu_id"] = maize_points["sample_id"].str.split("_").str[5] + "_" + maize_points["sample_id"].str.split("_").str[6]
+#group_by ssu_id
+maize_ssu = maize_points.groupby("ssu_id")["distance_to_pigeon_pea"].mean().reset_index()
+outfile = "/vitodata/worldcereal/data/COP4GEOGLAM/mozambique_pm/feature_distance/maize_distance_to_pigeon_pea.parquet"
+maize_ssu.to_parquet(outfile)
